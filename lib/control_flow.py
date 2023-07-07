f1e424c3e2ef7cd989eba4b1f66c42407f012b21
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-07-06 22:48:39 -0700

    Experiment with control flow.

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    access = "Access granted" if username.lower() == 'admin' and password == '12345' else "Access denied"
+    return access
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    operation_map = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y
+    }
+
+    if operation not in operation_map:
+        print("Invalid operation!")
+        return None
+    
+    return operation_map[operation](num1, num2)
